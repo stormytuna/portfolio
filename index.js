ddce90f55dd69e4224ddabc7d24e8233ffca5a20
stormytuna <stormytuna@outlook.com>
2023-02-28 01:12:46 +0000

    Refine dark mode, make it obey system default and make checkbox work better

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
 
+// Toggles dark mode
 document.getElementById("dark-mode-switch").addEventListener("click", (e) => {
 	const currentThemeIsDark = document.documentElement.getAttribute("data-bs-theme") === "dark";
 
@@ -10,9 +11,16 @@ document.getElementById("dark-mode-switch").addEventListener("click", (e) => {
 
 	document.body.style.backgroundImage = `url(${newBackground})`;
 	document.documentElement.setAttribute("data-bs-theme", newTheme);
-  if (currentThemeIsDark) {
-    document.documentElement.classList.remove("dark-mode")
-  } else {
-    document.documentElement.classList.add("dark-mode")
-  }
+	if (currentThemeIsDark) {
+		document.documentElement.classList.remove("dark-mode");
+	} else {
+		document.documentElement.classList.add("dark-mode");
+	}
 });
+
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+	document.body.style.backgroundImage = `url(assets/background-dark.jpg)`;
+	document.documentElement.setAttribute("data-bs-theme", "dark");
+	document.documentElement.classList.add("dark-mode");
+  document.getElementById("dark-mode-switch").checked = true
+}
